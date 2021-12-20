b4ccb1ea2abbc475265f2a829dd83506eb9fbced
Khoi Nguyen <nguyenvkhoi2002@gmail.com>
2021-12-20 12:13:48 +0700

    Revert "Mod Dog.js"
    This reverts commit dc58f068c8fd754a80e7f80cb6ab9f8b6e41cae5.

--- Dog.js
+++ Dog.js
@@ -1,8 +1,6 @@
 function Dog() {
   this.stomach = []
-  this.lung = ['oxygen', 'CO2']
   this.dead = false
-  this.favorite = []
 }
 
 Dog.prototype.eat = (cat) => {
@@ -15,8 +13,4 @@ Dog.prototype.sick = (virus) => {
 
 Dog.prototype.die = () => {
   this.dead = true
-}
-
-Dog.prototype.entertain = favorite => {
-  this.favorite.push(favorite)
 }
